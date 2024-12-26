9a8771e9127a390ff5e6817a7e28c9f4358e86d0
yommexg <hydrokidb@gmail.com>
2024-12-26 09:51:34 +0000

    update

--- src/config/corsOption.ts
+++ src/config/corsOption.ts
@@ -1,7 +1,6 @@
 const allowedOrigins = [
   "http://localhost:5173",
   "https://owomonie-admin-v1.vercel.app",
-  "https://www.google.com/",
 ];
 
 import { CorsOptions } from "cors";
@@ -12,14 +11,15 @@ const isAllowedOrigin = (origin: string | undefined): boolean => {
 };
 
 const corsOptions: CorsOptions = {
-  origin: (origin, callback) => {
-    if (!origin || !isAllowedOrigin(origin)) {
-      // Reject requests with disallowed origins
-      callback(new Error("Not allowed by CORS"));
-    } else {
-      callback(null, true);
-    }
-  },
+  // origin: (origin, callback) => {
+  //   if (!origin || !isAllowedOrigin(origin)) {
+  //     // Reject requests with disallowed origins
+  //     callback(new Error("Not allowed by CORS"));
+  //   } else {
+  //     callback(null, true);
+  //   }
+  // },
+  origin: true,
   optionsSuccessStatus: 200,
   credentials: true,
 };
