e833949524bb14a1d1c33a593052d9ba8db6aeaf
yommexg <hydrokidb@gmail.com>
2024-12-27 05:07:31 +0000

    update

--- src/config/corsOption.ts
+++ src/config/corsOption.ts
@@ -1,7 +1,5 @@
 const allowedOrigins = [
-  "exp+owomonie://expo-development-client/?url=http%3A%2F%2F192.168.179.251%3A8081",
-  "exp://192.168.179.251:8081",
-  "http://localhost:8081",
+  // "http://localhost:8081",
   "https://owomonie-admin-v1.vercel.app",
 ];
 
@@ -14,12 +12,10 @@ const isAllowedOrigin = (origin: string | undefined): boolean => {
 
 const corsOptions: CorsOptions = {
   origin: (origin, callback) => {
-    if (!origin || !isAllowedOrigin(origin)) {
-      console.log(origin);
-      // Reject requests with disallowed origins
-      callback(new Error("Not allowed by CORS"));
-    } else {
+    if (!origin || isAllowedOrigin(origin)) {
       callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
     }
   },
   optionsSuccessStatus: 200,
