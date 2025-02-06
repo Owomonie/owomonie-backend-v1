bb5ec1ba7017dcd8f7526528fd61f093862e98e7
yommexg <hydrokidb@gmail.com>
2025-02-06 03:50:30 +0000

    Create Index for public

--- src/server.ts
+++ src/server.ts
@@ -50,41 +50,6 @@ app.use(cookieParser());
 
 app.use(logger);
 
-app.get("/", (_req, res) => {
-  res.send(`<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="icon" type="image/png" href="/assets/logo2.png" />
-    <title>Owomonie API Call</title>
-    <style>
-      body {
-        background-color: #1772ab;
-        color: white;
-        font-family: Arial, sans-serif;
-        text-align: center;
-        padding: 50px;
-        margin: 0;
-      }
-      h1 {
-        font-size: 3em;
-        margin-bottom: 20px;
-      }
-      p {
-        font-size: 1.5em;
-      }
-    </style>
-  </head>
-  <body>
-  <img src="/assets/logo.png" alt="Owomonie Logo" class="logo" />
-    <h1>Welcome to Owomonie API calls</h1>
-    <p>This is the end point collection point for owomonie</p>
-  </body>
-</html>
-`);
-});
-
 app.use("/new-user-verification", userVerificationRoute);
 app.use("/register-user", userRegisterRoute);
 app.use("/login", userLoginRoute);
