250cd213b9956763d9600092ae02b1c0301e6d4e
yommexg <hydrokidb@gmail.com>
2025-02-12 11:21:50 +0000

    Update Bank Suffix

--- src/utils/bankSuffixRemove.ts
+++ src/utils/bankSuffixRemove.ts
@@ -1,4 +1,12 @@
 export default function removeBankSuffix(bankName: string) {
+  if (bankName.startsWith("The ")) {
+    bankName = bankName.slice(4);
+  }
+
+  if (bankName.startsWith("Bank of ")) {
+    bankName = bankName.slice(8);
+  }
+
   const firstSpaceIndex = bankName.indexOf(" ");
   if (firstSpaceIndex !== -1) {
     return bankName.slice(0, firstSpaceIndex);
