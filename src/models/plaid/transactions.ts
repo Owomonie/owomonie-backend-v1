53f9c117b863cc3c289b3329de3d844eab25034c
yommexg <hydrokidb@gmail.com>
2025-02-15 09:24:36 +0000

    Restructure Transaction Model

--- src/models/plaid/transactions.ts
+++ src/models/plaid/transactions.ts
@@ -1,30 +1,57 @@
 import mongoose, { Document, Schema } from "mongoose";
 
 export interface ITransaction extends Document {
-  date: Date;
-  name: string;
+  user: mongoose.Schema.Types.ObjectId;
+  account: mongoose.Schema.Types.ObjectId;
+  item: mongoose.Schema.Types.ObjectId;
   amount: number;
-  currency: "USD";
+  currency: string;
   isRemoved: boolean;
   type: "Debit" | "Credit";
-  description: string;
-  account: mongoose.Schema.Types.ObjectId; // Reference to Account
-  user: mongoose.Schema.Types.ObjectId; // Reference to User
-  categories: [string];
+  date: string;
+  // Not Reqd
+  transactionId: string;
+  accountId: string;
+  accountOwner: string;
+  category: string;
+  categoryId: string;
+  categoryLogo: string;
+  merchantId: string;
+  merchantName: string;
+  merchantLogo: string;
+  merchantWebsite: string;
+  paymentMode: string;
 }
 
 const transactionSchema: Schema<ITransaction> = new Schema({
-  date: { type: Date, required: true },
   amount: { type: Number, required: true },
   type: { type: String, required: true, enum: ["Debit", "Credit"] },
-  description: { type: String, required: true },
+  currency: { type: String, required: true },
   account: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "Account",
     required: true,
   },
+  item: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Item",
+    required: true,
+  },
   user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-  categories: { type: [String] },
+  date: { type: String, required: true },
+  isRemoved: { type: Boolean, required: true, default: false },
+
+  transactionId: { type: String },
+  accountId: { type: String },
+  accountOwner: { type: String },
+  category: { type: String },
+  categoryId: { type: String },
+  categoryLogo: { type: String },
+  merchantId: { type: String },
+  merchantName: { type: String },
+  merchantLogo: { type: String },
+  merchantWebsite: { type: String },
+  paymentMode: { type: String },
 });
 
 const Transaction = mongoose.model<ITransaction>(
