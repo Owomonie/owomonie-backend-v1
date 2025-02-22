44b677a23a6834eb43cc74069d703bdd775e348f
yommexg <hydrokidb@gmail.com>
2025-02-22 20:30:05 +0000

    Limit transaction sent

--- src/controllers/plaid-banks/get-user-bank-info.ts
+++ src/controllers/plaid-banks/get-user-bank-info.ts
@@ -123,7 +123,6 @@ export const handleGetUserTransaction = async (
           path: "accounts",
           populate: {
             path: "transactions",
-            options: { sort: { dateTime: -1 }, limit: 50 },
           },
         },
       })
